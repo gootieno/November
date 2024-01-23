6134c1b0f9c957b9faca7007ae05222ad8e87330
gootieno <gotieno@geoffreys-MBP.attlocal.net>
2024-01-23 09:59:18 -0800

    creating a server polya

--- Week8/Day2/02-formulate-a-response/server.js
+++ Week8/Day2/02-formulate-a-response/server.js
@@ -1,1 +1,16 @@
 // Your code here
+
+/*
+create http server
+- require packages etc. 
+
+create server instance
+    - get response body
+    - set headers
+    - set status code
+    - return res body (res.end())
+
+open port 
+
+listen to port
+*/
