c3d23b446e4637e7db2b9e90b9e24887862f898e
gootieno <gotieno@geoffreys-MBP.attlocal.net>
2024-01-23 13:32:24 -0800

    static assets  skeleton

--- Week8/Day2/04-serve-static-assets/server.js
+++ Week8/Day2/04-serve-static-assets/server.js
@@ -3,6 +3,15 @@ const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   // Your code here
+  if(req.method === 'GET' && req.url === '/'){
+    const responseBody = fs.readFileSync('./index.html', 'utf-8')
+
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html')
+    return res.end(responseBody)
+  }
+
+  // replace line 7, 10, and the req.url value for css and images
 });
 
 const port = 5000;
