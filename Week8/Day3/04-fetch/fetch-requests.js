db3c47c814486f498981d329b2219f7fb356369f
gootieno <gotieno@geoffreys-MBP.attlocal.net>
2024-01-24 14:28:08 -0800

    fetch requests

--- Week8/Day3/04-fetch/fetch-requests.js
+++ Week8/Day3/04-fetch/fetch-requests.js
@@ -7,31 +7,47 @@ Paste your code for fetch requests here once you finish each task.
 
 // Your code here
 
-
-
 /* ====== 2. Print true if the status of the response was successful ====== */
 
 // Your code here
 
-
-
 /* =================== 3. Print the Content-Type Header =================== */
 
 // Your code here
-
-
 
 /* ============== 4. Print the body of the response as text =============== */
 
 // Your code here
 
 const getBeautyProducts = async () => {
-    const response = await fetch('/categories/beauty/products')
-    console.log('response ', response)
+  const response = await fetch("/categories/beauty/products");
+  console.log("response ", response);
 
-    console.log('response status ', response.status)
+  console.log("response status ", response.status);
 
-    console.log('response text ', await response.text())
-}
+  console.log("response text ", await response.text());
+};
 
-getBeautyProducts()
+getBeautyProducts();
+
+const getProducts = async () => {
+  const response = await fetch("/products");
+
+  console.log("response ok? ", response.ok);
+  console.log("response headers ", response.headers.get("Content-Type"));
+  console.log("response text ", await response.text());
+};
+
+getProducts();
+
+fetch("/products",{
+    
+})
+  .then((response) => {
+    console.log("response ", response);
+    console.log("response ok? ", response.ok);
+    console.log("response headers ", response.headers.get("Content-Type"));
+
+    return response.text();
+  })
+  .then((data) => console.log(data));
