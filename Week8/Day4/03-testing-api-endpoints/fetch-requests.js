ab18ecc889173b9842d76b82c48cc82c3d640afc
gootieno <gotieno@geoffreys-MBP.attlocal.net>
2024-01-25 11:15:42 -0800

    fetch requests

--- Week8/Day4/03-testing-api-endpoints/fetch-requests.js
+++ Week8/Day4/03-testing-api-endpoints/fetch-requests.js
@@ -10,13 +10,36 @@ Paste your code for fetch requests here once you finish each task.
 */
 
 // Your code here
+const getPosts = () => {
+  return fetch("/posts")
+    .then((res) => res.json())
+    .then((resBody) => console.log(resBody));
+};
 
-
+getPosts()(async function () {
+  const res = await fetch("/posts");
+  const body = await res.json();
+  console.log(body);
+})();
 
 /* =============================== Phase 2 ================================ */
 /*
   Make a request with fetch request to POST /posts and print the response
   components to the console.
 */
+const createPost = async () => {
+  const response = await fetch("/posts", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      message: "New Post!",
+    }),
+  });
 
-// Your code here
+  const data = await response.json();
+  console.log(data);
+};
+
+createPost()
