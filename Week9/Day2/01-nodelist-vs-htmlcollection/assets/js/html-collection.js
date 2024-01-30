de9e22ab4b7020f77a5de86cd4c5a13e7d550617
gootieno <gotieno@geoffreys-MBP.attlocal.net>
2024-01-30 08:59:35 -0800

    cheatsheet

--- Week9/Day2/01-nodelist-vs-htmlcollection/assets/js/html-collection.js
+++ Week9/Day2/01-nodelist-vs-htmlcollection/assets/js/html-collection.js
@@ -1,10 +1,19 @@
 export default () => {
   const bodyChildElements = document.body.children; // HTMLCollection [div]
-  // console.log(bodyChildElements);
+  console.log(bodyChildElements);
 
   const div = bodyChildElements[0];
 
-  const divChildElements = div.children; // HTMLCollection [span]
+  const divChildElements = Array.from(div.children); // HTMLCollection [span]
+  
+  for(let i = 0; i < divChildElements.length; i++){
+    const span = document.createElement('span')
+    span.innerText = `${i+1}`
+
+    if(i === 1000) break
+    div.appendChild(span)
+  }
+
   const helloWorld = div.innerText; // Hello World! Yes!    <-- NOT Hello World!
   const span = divChildElements[0]; // <span>Yes!</span>
   // debugger
--- Week9/Day2/01-nodelist-vs-htmlcollection/assets/js/node-list.js
+++ Week9/Day2/01-nodelist-vs-htmlcollection/assets/js/node-list.js
@@ -5,6 +5,7 @@ export default () => {
   const div = bodyChildNodes[1]; // NOT bodyChildNodes[0]
 
   const divChildNodes = div.childNodes; // NodeList [text, span, text]
+  // console.log('div child nodes ', divChildNodes)
   const helloWorld = divChildNodes[0].textContent; // Hello World!\n
   const span = divChildNodes[1]; // <span>Yes!</span>
   // debugger
