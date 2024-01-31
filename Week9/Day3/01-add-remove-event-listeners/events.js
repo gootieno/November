14e8d56d51c1796ccc6ff826d3fbefe0beeb1888
Tanner Shaw <traumaaa@gmail.com>
2024-01-31 08:14:05 -0800

    w9d3 start

--- Week9/Day3/01-add-remove-event-listeners/events.js
+++ Week9/Day3/01-add-remove-event-listeners/events.js
@@ -1,1 +1,13 @@
 // Your code here
+
+window.addEventListener('DOMContentLoaded', (e) => {
+  // alert('DOM LOADED')
+
+
+  // <input value="red"></input>
+
+  // create the event on the input
+  // input.value
+  // if input.value === 'red'
+  // input.style.backgroundColor === 'red'
+})
